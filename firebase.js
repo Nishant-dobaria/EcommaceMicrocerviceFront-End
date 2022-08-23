a0af406310a721dd4bc84747ee168e6a09a554f2
Nishant Dobariya <nishantdobaria170@gmail.com>
2022-08-23 19:10:43 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyW3BPs3IKvKoo5iCiuGfBVU_pfUMETtw",
+    authDomain: "my-auth-890dd.firebaseapp.com",
+    projectId: "my-auth-890dd",
+    storageBucket: "my-auth-890dd.appspot.com",
+    messagingSenderId: "1024459113673",
+    appId: "1:1024459113673:web:be461fdfef39e67e137918"
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import Image from "next/image";
 import {MenuIcon,SearchIcon,ShoppingCartIcon} from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
   return (
     <header>
         <div className=" flex items-center bg-amazon_blue p-1 flex-grow py-2">
             <div className="mt-2 flex flex-grow sm:flex-grow-0">
-                <Image src="https://links.papareact.com/f90" width={150} height={40} objectFit="contain" className="cursor-pointer"/>
+                <h2 className="cursor-pointer text-6xl text-white font-bold p-2">WinkCommerce</h2>
+                {/* <Image src="https://links.papareact.com/f90" width={150} height={40} objectFit="contain" className="cursor-pointer"/> */}
             </div>
             <div className="hidden sm:flex items-center rounded-md h-10 bg-yellow-400 hover:bg-yellow-500 flex-grow cursor-pointer">
                 <input type="text" className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"/>
                 <SearchIcon className="h-12 p-4"/>
             </div>
             <div className="text-white items-center flex text-xs space-x-6 mx-6 whitespace-nowrap">
-                <div className="link">
+                <div onClick={signIn} className="link">
                     <p>Hello Nishant</p>
                     <p className="font-extrabold md:text-sm">Account & List</p>
                 </div>
